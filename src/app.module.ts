302d1d21832fb81d63892910d7dba53e7b8001df
izaanjahangir <izaanjahangir2@gmail.com>
2023-01-06 17:35:34 +0500

    added postgres configuration and User entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'izaan',
+      password: '12345678',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
